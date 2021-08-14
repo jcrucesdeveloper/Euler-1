f7db1733a798862f2a8696e6e982df9e6371ef43
jorge cruces <jorge.cruces@dsarhoya.cl>
2021-08-13 21:29:45 -0400

    Math View

--- app/src/main/java/com/jorgecruces/euler1/QuestionActivity.java
+++ app/src/main/java/com/jorgecruces/euler1/QuestionActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import io.github.kexanie.library.MathView;
+
+
 public class QuestionActivity extends AppCompatActivity {
+
+    private MathView mathView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+          mathView = (MathView) findViewById(R.id.mathView);
+
+        mathView.setText("<div style=\"font-size:30px;\"> $$2 + 2 = 4$$ </div>");
+
     }
 }
